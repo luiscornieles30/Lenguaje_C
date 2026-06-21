14fca51bdc252457898bb7587556523e4af5f372
Luis <luiscornieles30@gmail.com>
2026-06-21 14:07:32 -0400

    21/06/2026 (3er Commit)

--- practica.c
+++ practica.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main() {
+int main() { // Comentarios
     char texto[50];
     printf("Ingrese el siguente texto ");
-    scanf("%s", texto);
-    printf("El texto ingresado es: %s", texto);
+    scanf("%49[^\n]", texto);
+    printf("El texto ingresado es: %s", texto); 
     return 0;
 }
