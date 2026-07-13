92d5f020c03855c77244db9c9c683049796f4921
Luis <luiscornieles30@gmail.com>
2026-07-13 13:43:33 -0400

    13/07/2026 (4to Commit)

--- practica.c
+++ practica.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <conio.h>
 int main() { // Comentarios
     char texto[50];
+    int luisito;
+    printf("Ingrese un numero: ");
+    scanf("%d", &luisito);
     printf("Ingrese el siguente texto ");
-    scanf("%49[^\n]", texto);
-    printf("El texto ingresado es: %s", texto); 
+    scanf(" %49[^\n]", texto);
+    printf("El texto ingresado es: %s y el numero ingresado es %d", texto, luisito);
+    getch();
     return 0;
 }
